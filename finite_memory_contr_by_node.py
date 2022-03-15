Fix gradient order, name the edges, and adding SVD. Error appears.

finite_memory_contr_by_node.py
@@ -14,7 +14,8 @@ import tensornetwork as tn
 from utils_tn import initialized_lamdas_tn, gen_control_ten
 from utils_tn import edges_btw_ctr_nois, edges_in_lamdas
 from utils_tn import order4_to_2
-from utils_tn import rX, rY, contract_by_nodes
+from utils_tn import rX, rY, contract_by_nodes, noise_nonM_unitary
+from utils_tn import non_Markovian_unitary_map, rand_clifford_sequence_unitary_noise_list
 from nonM_analytical_expression import non_Markovian_theory_Fm
 
         
@@ -45,6 +46,7 @@ clifford_list = [ np.identity(2, dtype=complex),
 
 m = 6
 moves = 10
+lr = tn.Node(0.001)
 
 ket_0 = np.array([1,0], dtype=complex).reshape(2,1)
 ket_1 = np.array([0,1], dtype=complex).reshape(2,1)
@@ -56,7 +58,8 @@ proj_O = np.kron(ket_0, np.conj(ket_0.T))
 
 sys_dim = 2
 bond_dim = 2
-noise_u = np.identity(sys_dim * bond_dim, dtype=complex)
+# noise_u = np.identity(sys_dim * bond_dim, dtype=complex)
+noise_u = noise_nonM_unitary(m)
 
 lamdas = initialized_lamdas_tn(m, noise_u, rho_e)
 
@@ -67,7 +70,20 @@ for i in range(m):
 control_ten = gen_control_ten(rho_s, m, proj_O, rand_clifford)
 
 # -------------------- Generate the F_exp here -------------
-F_exp = 0.7*np.ones(m)
+F_exp = np.zeros(m)
+# F_exp, non-M unitary noise from Pedro's work.
+for lg in range(1, m+1):
+    # for i in range(sample_size):
+    tmp_rho, inver_op = rand_clifford_sequence_unitary_noise_list(lg, rho, noise_u, rand_clifford)
+    # 
+    tmp_rho = np.kron(I, inver_op) @ tmp_rho @ np.conj(np.kron(I, inver_op)).T
+    # final_state = noise_u @ tmp_rho @ np.conj(noise_u).T
+    final_state = non_Markovian_unitary_map(tmp_rho, noise_u[lg])
+    f_sys_state = np.trace(final_state.reshape(2,2,2,2), axis1=0, axis2=2)
+        # fm[i] = np.trace(proj_O @ f_sys_state).real
+        
+    F_exp[lg-1] = np.trace(proj_O @ f_sys_state).real
+# print(F_exp)
 
 # ----------------------------------------------------------
 F = np.zeros((moves, m))
@@ -75,6 +91,12 @@ test_f = np.zeros((moves, m))
 test_f_1 = np.zeros(moves)
 
 for k in range(moves):
+    for node in lamdas:
+        node.fresh_edges(node.axis_names)
+
+    for node in control_ten:
+        node.fresh_edges(node.axis_names)
+    
     # When updating the lams, also updating the lam_dgs.
     # i did not go to lam_0.
     i = k % (m+1)
@@ -91,6 +113,10 @@ for k in range(moves):
     for j in range(1,m+1):
         # This part has some redundent calculations, some F_pn did not change every time also they might not contribute to the cost func. But it will make things more complex.
         # Take the lamdas of sequence depth j and transform to order 2 tensor formate to calculate the analytical F.
+        """
+        Error here, don't know why yet.
+        """
+        
         noise_u = list(map(lambda x: order4_to_2(lamdas[x].tensor), np.arange(m-j,m+2)))
         analytical_F = non_Markovian_theory_Fm(proj_O, rho, I, sys_dim)
         F[k,j-1] = analytical_F.theory_Fm(j, noise_u[j-1])
@@ -107,10 +133,11 @@ for k in range(moves):
     # python: beta_{pn} tilde_Theta_{pn}^{i, i+1}, i = 0, ..., m
     tilde_theta_2 = np.zeros([2]*6, dtype=complex)
 
+    grad2 = tn.Node(np.zeros(L.shape))
     for l in range(i+1):
         pn = m-l
         exclude_2 = [i-l, i+1-l]
-        beta_pn = F_exp[pn-1] - F[k, pn-1]
+        beta_pn = tn.Node(F_exp[pn-1] - F[k, pn-1])
         if pn == m:
             tilde_ctr = control_ten
             tilde_lam = lamdas
@@ -163,17 +190,20 @@ for k in range(moves):
         tilde_theta_2.add_axis_names(tt2_name)
         tt2_reorder = list(map(lambda name: tilde_theta_2[name], L_axis_names))
         tilde_theta_2.reorder_edges(tt2_reorder)
-
         '''
         Since the code isn't finished, without contract all nodes, remaining some
         undangling edges would cause problem in loop.
         '''
-        con_ls = [tilde_theta_2] + exclude_nodes_2
-        test_f[k,pn-1] = contract_by_nodes(con_ls, None, 'f').tensor.real
-# print(np.matrix.round(test_f,2).real)
+        # con_ls = [tilde_theta_2] + exclude_nodes_2
+        # test_f[k,pn-1] = contract_by_nodes(con_ls, None, 'f').tensor.real
+        grad2 += beta_pn * tilde_theta_2
+    grad2.add_axis_names(tilde_theta_2.axis_names)
+    for x in range(len(tilde_theta_2.edges)):
+        grad2[x].set_name(tilde_theta_2.axis_names[x])
+
     if i+2 >= 1 and m+2-(i+2) >= 3:
         # exclude_1 = [0] # always the left most node.
-        beta_1 = F_exp[m-(i+2)-1] - F[k, m-(i+2)-1]
+        beta_1 = tn.Node(F_exp[m-(i+2)-1] - F[k, m-(i+2)-1])
         # Take the lamdas for tilde_Theta_(pn)
         noise_u = list(map(lambda x: order4_to_2(lamdas[x].tensor), np.arange(i+1, m+2)))
     
@@ -201,18 +231,48 @@ for k in range(moves):
         lam_ex_1 = edges_in_lamdas(tilde_lam, m-(i+1))
         ctr_ex_1 = edges_btw_ctr_nois(tilde_ctr, tilde_lam, m-(i+1))
         tilde_theta_1 = contract_by_nodes(tilde_lam[1:]+tilde_ctr, None, 'tilde_theta_1')
-
-        test_f_1[k] = contract_by_nodes([tilde_theta_1]+[tilde_lam[0]], None, 'test_f_1').tensor.real
-print(np.matrix.round(test_f_1,2).real)
-
-        # tilde_theta_2 += beta * tmp_theta_2.tensor
-        # ============ tilde_theta_2 is done, now a tilde_theta_1 =========================
-        # tilde_theta_1 = contract_edge_list(lam_ex_1)
-        # tilde_theta_1 = contract_edge_list(ctr_ex_1, name='theta1_'+str(m+1-l))
         
-        # dL = (F_exp[j] - F[k,j]) * Theta_2(j+) + (F_exp[j+2] - F[k,j+2]) * Theta_1(i+1)
+        tt1_name = list(map(lambda edg: edg.name, tilde_theta_1.get_all_edges()))
+        tilde_theta_1.add_axis_names(tt1_name)
+
+        tilde_theta_1.fresh_edges(tilde_theta_1.axis_names)
+        ele_inv_lam = tn.Node(1) / lamdas[i]
+        for x in range(len(ele_inv_lam.edges)):
+            ele_inv_lam[x].set_name(lamdas[i].axis_names[x])
+        ele_inv_lam.set_name('inv')
+        ele_inv_lam.add_axis_names(lamdas[i].axis_names)
+        cont_edg = list(set(lamdas[i].axis_names) & set(tilde_theta_1.axis_names))[0]
+        ele_inv_lam[cont_edg] ^ tilde_theta_1[cont_edg]
+        # tilde1 = ele_inv_lam @ tilde_theta_1
+        tilde_theta_1 = tilde_theta_1 @ ele_inv_lam
+        tilde1_name = list(map(lambda edg: edg.name, tilde_theta_1.get_all_edges()))
+        tilde_theta_1.add_axis_names(tilde1_name)
+        grad1 = beta_1 * tilde_theta_1
+        for x in range(len(tilde_theta_1.edges)):
+            grad1[x].set_name(tilde_theta_1[x].name)
+        grad1.add_axis_names(tilde_theta_1.axis_names)
+
+        # test_f_1[k] = contract_by_nodes([tilde_theta_1]+[tilde_lam[0]], None, 'test_f_1').tensor.real
+    testa = grad1.tensor[0,0,0,0]
+    # Reordering grad1 and grad2.
+    grad1_reorder = list(map(lambda name: grad1[name], grad2.axis_names))
+    grad1.reorder_edges(grad1_reorder)
+    testb = grad1.tensor[0,0,0,0]
+    L -= lr * (grad2 + grad1)
+    for x in range(len(L.edges)):
+        L[x].set_name(grad1[x].name)
+    L.add_axis_names(grad1.axis_names)
     
-    # '''
-    # math: tilde_theta^{i-1}
-    # just copy from above, make sure it is correct before working on it.
-    # '''
+    # SVD on L
+    u_prime, vh_prime, _ = tn.split_node(L, left_edges=[L[0],L[1],L[2]], right_edges=[L[3],L[4],L[5]])
+    u_prime.set_name(lamdas[i].name)
+    u_prime.add_axis_names(lamdas[i].axis_names)
+    u_prime[-1].set_name(lamdas[i].axis_names[-1])
+    vh_prime.set_name(lamdas[i+1].name)
+    vh_prime.add_axis_names(lamdas[i+1].axis_names)
+    vh_prime[0].set_name(lamdas[i+1].axis_names[0])
+    lamdas[i] = u_prime
+    lamdas[i+1] = vh_prime
+# Note here, the order of test_f_1 and test_f is not the same.
+# print('test_f', np.matrix.round(test_f,4).real)
+# print('test_f_1', np.matrix.round(test_f_1,4).real)